e33ca8136f2252df7e9fc229a801748d6cc4f937
Austin Powell <austinpowell58@gmail.com>
2018-06-21 02:48:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine (array) {
   return "The line is currently empty.";
   else
   var i = 0;
-  while (i <= array.length) {
+  while (i < array.length) {
   line.push((i + 1) + ". " + array[i]) + " ";
   i++;
  }
