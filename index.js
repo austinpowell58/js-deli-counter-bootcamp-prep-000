f2921dc31a2fae359072c6965a1ecad3508819ea
Austin Powell <austinpowell58@gmail.com>
2018-06-21 02:25:16 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+var katzDeli = [];
+function takeANumber(array, person) {
+  array.push(person);
+  return "Welcome, " + person + ". You are number " + array.length + " in line.";
+}
+
+function nowServing (array) {
+  {if (array.length === 0)
+  return "There is nobody waiting to be served!";
+  else 
+  katzDeli.pop();
+  return "Currently serving " + array[0] + ".";
+  }
+}
+function currentLine (array) {
+  var line = [];
+  if (array.length === 0)
+  return "The line is currently empty.";
+  else
+ { var i = 0;
+  while (i <= array.length)
+  line.push((i + 1) + ". " + array[i]);
+  i++;
+ }
+ return "The line is currently: " + line;
+}
